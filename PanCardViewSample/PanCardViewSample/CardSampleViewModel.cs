41c3d802832a53622b0b23b6e580d659226822cf
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2018-01-15 01:33:28 +0300

    updated sample

---
+++ PanCardViewSample/PanCardViewSample/CardSampleViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace PanCardViewSample
+{
+    public sealed class PanCardSampleViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _currentIndex;
+        private int _ImageCount = 500;
+
+        public PanCardSampleViewModel()
+        {
+            Items = new ObservableCollection<object>
+            {
+                new { Source = CreateSource() },
+                new { Source = CreateSource() },
+                new { Source = CreateSource() },
+                new { Source = CreateSource() },
+                new { Source = CreateSource() }
+            };
+        }
+
+        public int CurrentIndex
+        {
+            get => _currentIndex;
+            set
+            {
+                if (value + 1 >= Items.Count)
+                {
+                    Items.Add(new
+                    {
+                        Source = CreateSource()
+                    });
+                    Items.RemoveAt(0);
+                }
+                _currentIndex = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentIndex)));
+            }
+        }
+
+        public ObservableCollection<object> Items { get; }
+
+        private string CreateSource()
+        {
+            _ImageCount++;
+            return $"http://lorempixel.com/400/400/sports/IMAGE{_ImageCount}/";
+        }
+    }
+}
---
+++ PanCardViewSample/PanCardViewSample/CardsSampleView.cs
@@ -0,0 +1,53 @@
+﻿// 11(c) Andrei Misiukevich
+using System;
+using FFImageLoading.Forms;
+using PanCardView;
+using Xamarin.Forms;
+namespace PanCardViewSample
+{
+    public class CardsSampleView : ContentPage
+    {
+        public CardsSampleView()
+        {
+            var cardsView = new CardsView
+            {
+                ItemViewFactory = new CardViewItemFactory(RuleHolder.Rule),
+                BackgroundColor = Color.Black.MultiplyAlpha(.9)
+            };
+            cardsView.SetBinding(CardsView.ItemsProperty, nameof(PanCardSampleViewModel.Items));
+            cardsView.SetBinding(CardsView.CurrentIndexProperty, nameof(PanCardSampleViewModel.CurrentIndex));
+
+            Title = "PanCardViewSample";
+            Content = cardsView;
+            BindingContext = new PanCardSampleViewModel();
+        }
+    }
+
+    public static class RuleHolder
+    {
+        public static CardViewFactoryRule Rule { get; } = new CardViewFactoryRule
+        {
+            Creator = () =>
+            {
+                var content = new AbsoluteLayout();
+                var frame = new Frame
+                {
+                    Padding = 0,
+                    HasShadow = false,
+                    CornerRadius = 10,
+                    IsClippedToBounds = true
+                };
+                content.Children.Add(frame, new Rectangle(.5, .5, 300, 300), AbsoluteLayoutFlags.PositionProportional);
+
+                var image = new CachedImage
+                {
+                    Aspect = Aspect.AspectFill
+                };
+                image.SetBinding(CachedImage.SourceProperty, "Source");
+
+                frame.Content = image;
+                return content;
+            }
+        };
+    }
+}
