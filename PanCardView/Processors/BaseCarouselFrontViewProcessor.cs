7b45550733ce4f7a6a365ee07d875aaeb5b385ca
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2018-01-19 01:08:35 +0300

    added initial carousel processors

---
+++ PanCardView/Processors/BaseCarouselFrontViewProcessor.cs
@@ -0,0 +1,31 @@
+﻿// 11(c) Andrei Misiukevich
+using System;
+using System.Threading.Tasks;
+using PanCardView.Enums;
+using Xamarin.Forms;
+
+namespace PanCardView.Processors
+{
+    public class BaseCarouselFrontViewProcessor : ICardProcessor
+    {
+        public Task HandlePanApply(View view, PanItemPosition panItemPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandlePanChanged(View view, double xPos, PanItemPosition panItemPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HandlePanReset(View view, PanItemPosition panItemPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitView(View view, PanItemPosition panItemPosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
