be890d7ffa06386cd0c0b2aeb8214f56b4533e4a
Andrei <andrei.misiukevich@gmail.com>
2018-03-20 02:01:12 +0300

    opened utility classes

--- PanCardView/Utility/ViewsInUseSet.cs
+++ PanCardView/Utility/ViewsInUseSet.cs
@@ -4,31 +4,31 @@ using Xamarin.Forms;
 
 namespace PanCardView.Utility
 {
-	internal class ViewsInUseSet
+	public sealed class ViewsInUseSet
 	{
-		private readonly Dictionary<View, int> _set = new Dictionary<View, int>();
+		private readonly Dictionary<View, int> _viewsSet = new Dictionary<View, int>();
 
-		internal void Add(View view) 
-		=> _set[view] = Contains(view)
-			? _set[view] + 1
+		public void Add(View view) 
+		=> _viewsSet[view] = Contains(view)
+			? _viewsSet[view] + 1
 			: 1;
 
-		internal bool Remove(View view)
+		public bool Remove(View view)
 		{
 			if (!Contains(view))
 			{
 				return false;
 			}
 
-			var currentCount = _set[view] - 1;
+			var currentCount = _viewsSet[view] - 1;
 			if (currentCount > 0)
 			{
-				_set[view] = currentCount;
+				_viewsSet[view] = currentCount;
 			}
 			return true;
 		}
 
-		internal bool Contains(View view) 
-		=> view != null && _set.ContainsKey(view);
+		public bool Contains(View view) 
+		=> view != null && _viewsSet.ContainsKey(view);
 	}
 }
