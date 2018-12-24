b24ddcb3574792f997e77174ba8dc7e2a875df67
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2018-12-25 01:05:58 +0300

    Added MacOS project, updated Forms version

---
+++ PanCardViewSample/MacOS/AppDelegate.cs
@@ -0,0 +1,36 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+using FFImageLoading.Forms.Platform;
+
+namespace PanCardViewSample.MacOS
+{
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        private readonly NSWindow _window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "CardsView";
+            _window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow
+        {
+            get { return _window; }
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            CachedImageRenderer.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+    }
+}
