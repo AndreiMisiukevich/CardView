23a7ad5d0242b68aadf76471bcf83b94afcaa69a
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2019-05-12 01:01:30 +0300

    sample updates

--- PanCardViewSample/PanCardViewSample/Views/CoverFlowSampleXamlView.xaml.cs
+++ PanCardViewSample/PanCardViewSample/Views/CoverFlowSampleXamlView.xaml.cs
@@ -23,7 +23,7 @@ namespace PanCardViewSample.Views
                 Icon = "next",
                 CommandParameter = true
             };
-            nextItem.SetBinding(MenuItem.CommandProperty, nameof(CardsSampleViewModel.PanPositionChangedCommand));
+            nextItem.SetBinding(MenuItem.CommandProperty, nameof(CardsSampleViewModel.RemoveCurrentItemCommand));
 
             ToolbarItems.Add(prevItem);
             ToolbarItems.Add(nextItem);
