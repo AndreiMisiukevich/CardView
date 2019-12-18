6d5f15ddf2852802a987e037a3462b26ffdd4875
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2019-12-18 11:52:03 +0300

    hided parentScrollView

--- PanCardView/Controls/ParentScrollView.cs
+++ PanCardView/Controls/ParentScrollView.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace PanCardView.Controls
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete("This control is obsolete/deprecated. Please use regular ScrollView instead.")]
     public class ParentScrollView : ScrollView
     {
