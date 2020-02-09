cc6f546bc8c24877a4b2aba6a34d4182bd7515be
Pavel Kurianov <kurianov.pavel@gmail.com>
2020-02-09 18:07:59 +0200

    Add stripe position property for TabsControl

---
+++ PanCardView/Enums/StripePostion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PanCardView.Enums
+{
+    public enum StripePosition
+    {
+        Top,
+        Bottom
+    }
+}
