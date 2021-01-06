9c191f8aa06ce500a6c00ed79aeb0fb790af0f7f
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2021-01-06 04:43:40 +0300

    https://github.com/AndreiMisiukevich/CardView/issues/326

--- PanCardView/Processors/CubeProcessor.cs
+++ PanCardView/Processors/CubeProcessor.cs
@@ -8,22 +8,9 @@ namespace PanCardView.Processors
     public class CubeProcessor : CarouselProcessor
     {
         protected override double GetTranslationX(View view, CardsView cardsView)
-        {
-            if (view == null)
-            {
-                return 0;
-            }
-
-            var value = cardsView.IsHorizontalOrientation
-                ? view.Margin.Left > 0
-                    ? view.Margin.Left
-                    : -view.Margin.Right
-                : view.Margin.Top > 0
-                    ? view.Margin.Top
-                    : -view.Margin.Bottom;
-            value += Sign(value) * cardsView.GetSize(view) * 0.5 * (1 - view.Scale);
-            return value;
-        }
+            => view != null
+            ? (cardsView.IsHorizontalOrientation ? view.RotationY : view.RotationX) * cardsView.GetSize() / Angle90
+            : 0;
 
         protected override void SetTranslationX(View view, double value, CardsView cardsView, bool isFront, bool? isVisible = null, bool isClean = false)
         {
@@ -39,6 +26,22 @@ namespace PanCardView.Processors
                 view.Opacity = CalculateFactoredProperty(value, OpacityFactor, cardsView);
                 var margin = value - Sign(value) * cardsView.GetSize(view) * 0.5 * (1 - view.Scale);
                 var rotation = value * Angle90 / cardsView.GetSize();
+
+                if (Device.RuntimePlatform == Device.Android)
+                {
+                    var anchor = .5 * (1 - Sin(rotation * PI / Angle180));
+                    if (cardsView.IsHorizontalOrientation)
+                    {
+                        view.AnchorX = anchor;
+                        view.AnchorY = .5;
+                    }
+                    else
+                    {
+                        view.AnchorX = .5;
+                        view.AnchorY = anchor;
+                    }
+                }
+
                 if (cardsView.IsHorizontalOrientation)
                 {
                     view.Margin = new Thickness(margin > 0 ? margin : 0, 0, margin < 0 ? -margin : 0, 0);
