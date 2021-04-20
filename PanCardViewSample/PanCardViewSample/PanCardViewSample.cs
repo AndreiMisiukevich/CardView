857a3597710049987a42b0fa92aa34e0efb5d5c1
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2021-04-20 20:59:12 +0200

    bump 2.7.5

--- PanCardViewSample/PanCardViewSample/PanCardViewSample.cs
+++ PanCardViewSample/PanCardViewSample/PanCardViewSample.cs
@@ -3,73 +3,73 @@ using Xamarin.Forms;
 
 namespace PanCardViewSample
 {
-	public class App : Application
-	{
-		public App()
-		{
+    public class App : Application
+    {
+        public App()
+        {
             if (Device.RuntimePlatform == Device.GTK)
             {
                 MainPage = new Views.Gtk.CoverFlowSampleXamlView();
                 return;
             }
             MainPage = new NavigationPage(new StartPage());
-		}
-	}
+        }
+    }
 
-	public class StartPage : ContentPage
-	{
-		public StartPage()
-		{
-			Title = "PanCardViewSample";
-			BackgroundColor = Color.White;
+    public class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            Title = "PanCardViewSample";
+            BackgroundColor = Color.White;
 
-			var toCardsBtn = new Button { Text = "CardsView Items", FontSize = 20, TextColor = Color.Black };
-			toCardsBtn.Clicked += (sender, e) =>
-			{
-				this.Navigation.PushAsync(new CardsSampleView());
-			};
+            var toCardsBtn = new Button { Text = "CardsView Items", FontSize = 20, TextColor = Color.Black };
+            toCardsBtn.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new CardsSampleView());
+            };
 
-			var toCoverFlowBtn = new Button { Text = "CoverFlowView", FontSize = 20, TextColor = Color.Black };
+            var toCoverFlowBtn = new Button { Text = "CoverFlowView", FontSize = 20, TextColor = Color.Black };
             toCoverFlowBtn.Clicked += (sender, e) =>
-			{
+            {
                 this.Navigation.PushAsync(new CoverFlowSampleXamlView());
-			};
+            };
 
-			var toCarouselNestedBtn = new Button { Text = "CarouselView Nested" };
-			toCarouselNestedBtn.Clicked += (sender, e) =>
-			{
-				this.Navigation.PushAsync(new CarouselSampleNestedXamlView());
-			};
+            var toCarouselNestedBtn = new Button { Text = "CarouselView Nested" };
+            toCarouselNestedBtn.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new CarouselSampleNestedXamlView());
+            };
 
-			var toCarouselScrollBtn = new Button { Text = "CarouselView scroll" };
-			toCarouselScrollBtn.Clicked += (sender, e) =>
-			{
-				this.Navigation.PushAsync(new CarouselSampleSrollView());
-			};
+            var toCarouselScrollBtn = new Button { Text = "CarouselView scroll" };
+            toCarouselScrollBtn.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new CarouselSampleSrollView());
+            };
 
-			var toCarouselNoTemplateBtn = new Button { Text = "CarouselView No template" };
-			toCarouselNoTemplateBtn.Clicked += (sender, e) =>
-			{
-				this.Navigation.PushAsync(new CarouselSampleViewNoTemplate());
-			};
+            var toCarouselNoTemplateBtn = new Button { Text = "CarouselView No template" };
+            toCarouselNoTemplateBtn.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new CarouselSampleViewNoTemplate());
+            };
 
-			var toCarouselXamlBtn = new Button { Text = "CarouselView Xaml", FontSize = 20, TextColor = Color.Black };
-			toCarouselXamlBtn.Clicked += (sender, e) =>
-			{
-				this.Navigation.PushAsync(new CarouselSampleXamlView());
-			};
+            var toCarouselXamlBtn = new Button { Text = "CarouselView Xaml", FontSize = 20, TextColor = Color.Black };
+            toCarouselXamlBtn.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new CarouselSampleXamlView());
+            };
 
-			var toCarouselListBtn = new Button { Text = "Carousel ListView" };
-			toCarouselListBtn.Clicked += (sender, e) =>
-			{
-				this.Navigation.PushAsync(new CarouselSampleListView());
-			};
+            var toCarouselListBtn = new Button { Text = "Carousel ListView" };
+            toCarouselListBtn.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new CarouselSampleListView());
+            };
 
-			var toCarouselEmbBtn = new Button { Text = "Carousel Embedded views" };
-			toCarouselEmbBtn.Clicked += (sender, e) =>
-			{
-				this.Navigation.PushAsync(new CarouselSampleEmbeddedView());
-			};
+            var toCarouselEmbBtn = new Button { Text = "Carousel Embedded views" };
+            toCarouselEmbBtn.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new CarouselSampleEmbeddedView());
+            };
 
             var toCubeBtn = new Button { Text = "CubeView Xaml", FontSize = 20, TextColor = Color.Black };
             toCubeBtn.Clicked += (sender, e) =>
@@ -84,24 +84,24 @@ namespace PanCardViewSample
             };
 
             Content = new ScrollView
-			{
-				Content = new StackLayout
-				{
-					Margin = 20,
-					Children = {
-						toCardsBtn,
-						toCarouselXamlBtn,
-						toCoverFlowBtn,
-						toCubeBtn,
-						toCarouselNestedBtn,
-						toCarouselScrollBtn,
-						toCarouselNoTemplateBtn,
-						toCarouselListBtn,
-						toCarouselEmbBtn,
+            {
+                Content = new StackLayout
+                {
+                    Margin = 20,
+                    Children = {
+                        toCardsBtn,
+                        toCarouselXamlBtn,
+                        toCoverFlowBtn,
+                        toCubeBtn,
+                        toCarouselNestedBtn,
+                        toCarouselScrollBtn,
+                        toCarouselNoTemplateBtn,
+                        toCarouselListBtn,
+                        toCarouselEmbBtn,
                         toXFIndicatorViewBtn
                     }
-				}
-			};
+                }
+            };
         }
-	}
+    }
 }
